1adae4144acebf095d257996eda603a7bdf34cfa
Zhou, Panxing (CA - Toronto) <pazhou@deloitte.ca>
2019-01-18 17:23:58 -0500

    refactoring

--- app_client/src/index.tsx
+++ app_client/src/index.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './app/styles/vars.style';
 import 'semantic-ui-css/semantic.min.css';
+
 import './index.css';
+import { theme } from './app/styles/vars.style';
 
 import App from './app/App';
 import ErrorBoundary from './app/ErrorBoundary';
-
 import repo from './service/repository';
-
 repo.getLocalForm().subscribe(res => {
   // console.log(JSON.stringify(res, null, 2));
 });
@@ -24,13 +23,13 @@ const store = configureStore();
 class AppComponent extends React.Component {
   public render() {
     return (
-      <Router>
+      <BrowserRouter>
         <ThemeProvider theme={theme}>
           <Switch>
             <Route path="/" component={App} />
           </Switch>
         </ThemeProvider>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
