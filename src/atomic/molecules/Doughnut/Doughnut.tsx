b5a76a8407f3b52376f9d7930418d573485f63e6
Iulianna Gevchuk <igevchuk@deloitte.ca>
2019-04-10 17:28:41 -0400

    Convert donut chart boolean options to functions

--- src/atomic/molecules/Doughnut/Doughnut.tsx
+++ src/atomic/molecules/Doughnut/Doughnut.tsx
@@ -3,12 +3,12 @@ import DonutChart from 'react-donut-chart';
 
 export default ({ data, colors, options: propOptions }) => {
   const getDefaultOptions = () => ({
-    clickToggle: false,
+    clickToggle: () => false,
     innerRadius: 0.5,
     height: 120,
     legend: false,
-    onClick: false,
-    onMouseEnter: false,
+    onClick: () => false,
+    onMouseEnter: () => false,
     outerRadius: 1,
     selectedOffset: false,
     startAngle: 0,
